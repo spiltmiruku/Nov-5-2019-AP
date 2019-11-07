a50ada98d0997019f40af83399d5b3cfae3ebcc4
spiltmiruku <spiltmiruku@gmail.com>
2019-11-06 23:05:25 -0700

    Finished Tic Tac Toe board (not including black diamond problems)

--- vanillajs-1-afternoon/Part1/index.js
+++ vanillajs-1-afternoon/Part1/index.js
@@ -1,6 +1,6 @@
-console.log("tic tac toe script connected")
+console.log("tic tac toe script connected");
 
-var board = []
+var board = [];
 
 function play(clickedId) {
   const playerSpan = document.getElementById("player");
@@ -13,6 +13,80 @@ function play(clickedId) {
   } else {
     playerSpan.innerText = "X";
     clickedElement.innerText = "O";
-    board[clickedID] = "O"
+    board[clickedID] = "O";
+  }
+  console.log(board);
+
+  const topLeft = board[0];
+  const topCenter = board[1];
+  const topRight = board[2];
+  const midLeft = board[3];
+  const midCenter = board[4];
+  const midRight = board[5];
+  const botLeft = board[6];
+  const botCenter = board[7];
+  const botRight = board[8];
+
+  if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
+    alert(`${topLeft} is the winner!`);
+    return;
+  }
+
+  if (midLeft !== undefined && midLeft === midCenter && midLeft === midRight) {
+    alert(`${midLeft} is the winner!`);
+    return;
+  }
+  if (botLeft !== undefined && botLeft === botCenter && botLeft === botRight) {
+    alert(`${botLeft} is the winner!`);
+    return;
+  }
+  if (topLeft !== undefined && midLeft === botLeft && topLeft === botLeft) {
+    alert(`${topLeft} is the winner!`);
+    return;
+  }
+  if (
+    topCenter !== undefined &&
+    topCenter === midCenter &&
+    topCenter === botCenter
+  ) {
+    alert(`${topCenter} is the winner!`);
+    return;
+  }
+  if (
+    topRight !== undefined &&
+    topRight === midRight &&
+    topRight === botRight
+  ) {
+    alert(`${topRight} is the winner!`);
+    return;
+  }
+  if (topLeft !== undefined && topLeft === midCenter && topLeft === botRight) {
+    alert(`${topLeft} is the winner!`);
+    return;
+  }
+  if (
+    topRight !== undefined &&
+    topRight === midCenter &&
+    topRight === botLeft
+  ) {
+    alert(`${topRight} is the winner!`);
+    return;
+  }
+
+  let boardFull = true;
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === undefined) {
+      boardFull = false;
+    }
+  }
+  if (boardFull === true) {
+    alert("It's a Cat's game! Tied game");
   }
 }
+
+
+//Black Diamond
+// 1. Board reset function
+// 2. Board reset button
+// 3. Override a square that has been played + override
+// 4. Stops the game after winner declared
